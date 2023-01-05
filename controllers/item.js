9356c4b2d0f2f2ae4ddcbe9fd99465a932a2b6d9
Mert <mertozpolatt@gmail.com>
2023-01-05 12:53:35 +0300

    Update and delete item funcs.

--- controllers/item.js
+++ controllers/item.js
@@ -21,6 +21,27 @@ const getAllItems = asyncErrorWrapper(async (req, res, next) => {
         data: item
     });
 });
+const updateItem= asyncErrorWrapper(async (req,res,next)  => {
+    const {id} = req.params;
+    const {name,image,info} = req.body;
+    let item = await Item.findById(id);
+    item.name=name;
+    item.image=image;
+    item.info=info;
+    item = await item.save();
+    res.status(200).json({
+        success:true,
+        data:item
+    });
+});
+const deleteItem= asyncErrorWrapper(async (req,res,next)  => {
+    const {id} = req.params;
+    await Item.findByIdAndDelete(id);
+    res.status(200).json({
+        success:true,
+        data:"deleted"
+    });
+});
 module.exports = {
-    addItem, getAllItems
+    addItem, getAllItems,deleteItem,updateItem
 }
--- routers/item.js
+++ routers/item.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { addItem, getAllItems } = require("../controllers/item");
+const { addItem, getAllItems ,updateItem,deleteItem} = require("../controllers/item");
 const router = express.Router();
 
 
 router.post("/additem",addItem);
 router.get("/",getAllItems);
+router.put("/:id/update",updateItem);
+router.delete("/:id/delete",deleteItem);
 
 module.exports = router;
